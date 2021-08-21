8e76f5c809ea3ab1fdd1c62b3e78717c2b8b0946
Kim Weible <kw00183@my.westga.edu>
2021-08-21 16:16:13 -0500

    BabbleGuiController.java - split up the methods to distribute the handle and set and get logic better, and add in else statement to inner class to setText to empty

--- src/main/java/edu/westga/cs/babble/controllers/BabbleGuiController.java
+++ src/main/java/edu/westga/cs/babble/controllers/BabbleGuiController.java
@@ -4,6 +4,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -11,11 +12,14 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.util.Callback;
 import edu.westga.cs.babble.model.EmptyTileBagException;
 
 import edu.westga.cs.babble.model.Tile;
 import edu.westga.cs.babble.model.TileBag;
+import edu.westga.cs.babble.model.TileGroup;
+import edu.westga.cs.babble.model.TileNotInGroupException;
 import edu.westga.cs.babble.model.TileRack;
 
 /**
@@ -57,7 +61,9 @@ public class BabbleGuiController implements Initializable {
 		this.textFieldScore.setText("0");
 
 		try {
-			this.listViewTiles = this.getTiles();
+			this.listViewTiles = this.getRandomTiles();
+			this.setTiles();
+			this.clickTiles();
 		} catch (EmptyTileBagException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -68,32 +74,54 @@ public class BabbleGuiController implements Initializable {
 		this.textFieldYourWord.setText("");
 	}
 
-	private ListView<Tile> getTiles() throws EmptyTileBagException {
+	private ListView<Tile> getRandomTiles() throws EmptyTileBagException {
 		for (int index = 0; index < TileRack.MAX_SIZE; index++) {
 			Tile randomTile = this.tileBag.drawTile();
 			this.tileRack.append(randomTile);
 		}
-		ObservableList<Tile> tiles = this.tileRack.tiles();
-
-		this.listViewTiles.setItems(tiles);
-
-		this.listViewTiles.setCellFactory(
-				new Callback<ListView<Tile>, ListCell<Tile>>() {
-					@Override
-					public ListCell<Tile> call(ListView<Tile> tiles) {
-						return new TileCharacterCell();
-					}
-				});
-
 		return this.listViewTiles;
 	}
 	
+	private void setTiles() {
+		ObservableList<Tile> tiles = this.tileRack.tiles();
+		this.listViewTiles.setItems(tiles);
+
+		this.listViewTiles.setCellFactory(new Callback<ListView<Tile>, ListCell<Tile>>() {
+			@Override
+			public ListCell<Tile> call(ListView<Tile> tiles) {
+				return new TileCharacterCell();
+			}
+		});
+	}
+	
+	private void clickTiles() {
+		this.listViewTiles.setOnMouseClicked(new EventHandler<MouseEvent>(){
+			@Override
+			public void handle(MouseEvent arg0) {
+				Tile clickedTile = BabbleGuiController.this.listViewTiles.getSelectionModel().getSelectedItem();
+				try {
+					BabbleGuiController.this.tileRack.remove(clickedTile);
+					
+					BabbleGuiController.this.setTiles();
+					
+					ObservableList<Tile> tiles = BabbleGuiController.this.tileRack.tiles(); 
+					System.out.println(tiles);
+				} catch (TileNotInGroupException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	          }
+		});
+	}
+
 	static class TileCharacterCell extends ListCell<Tile> {
-        public void updateItem(Tile item, boolean empty) {
-            super.updateItem(item, empty);
-            if (item != null) {
-            	setText(String.valueOf(item.getLetter()));
+		public void updateItem(Tile item, boolean empty) {
+			super.updateItem(item, empty);
+			if (item != null) {
+				setText(String.valueOf(item.getLetter()));
+			} else {
+                setText("");
             }
-        }
-    }
+		}
+	}
 }
